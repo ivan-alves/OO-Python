2b927f2876409676bcd5723e490c324ee16c2d10
Ivan Alves <esilva.ivan@gmail.com>
2021-02-17 10:25:00 -0300

    aula 1 e 2

---
+++ conta.py
@@ -0,0 +1,10 @@
+
+
+class Conta:
+
+    # construtor
+    def __init__(self, numero, titular, saldo, limite):
+        self.numero = numero
+        self.titular = titular
+        self.saldo = saldo
+        self.limite = limite
---
+++ teste_conta.py
@@ -0,0 +1,6 @@
+from conta import Conta
+
+conta = Conta( 1, "Ivan", 200.0, 1000.0)
+print(conta)
+
+
